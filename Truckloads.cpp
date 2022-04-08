5483156d74a17365773d7c4278a5bd4230782529
pranxyz <65145297+pranxyz@users.noreply.github.com>
2022-04-08 13:50:10 +0930

    Update Truckloads.cpp

--- Truckloads.cpp
+++ Truckloads.cpp
@@ -5,7 +5,7 @@
 int Truckloads::numTrucks(int numCrates, int loadSize)
 {
     if(numCrates <= loadSize)
-    return 1;
+    return -1;
     if(numCrates % 2)
     return numTrucks(numCrates/2 + 1, loadSize) + numTrucks(numCrates/2, loadSize);
     return 2 * numTrucks(numCrates/2, loadSize);
