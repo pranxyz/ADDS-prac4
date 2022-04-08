24499ece18b01cfe442d8aec9eafc88213eced3e
pranxyz <65145297+pranxyz@users.noreply.github.com>
2022-04-08 13:54:28 +0930

    Update EfficientTruckloads.cpp

--- EfficientTruckloads.cpp
+++ EfficientTruckloads.cpp
@@ -4,8 +4,10 @@
 
 int EfficientTruckloads::numTrucks(int numCrates, int loadSize)
 {
-    if(numCrates <= loadSize)
+    if(loadSize <= 0)
     return -1;
+    if(numCrates <= loadSize)
+    return 1;
     if(numCrates % 2)
     return numTrucks(numCrates/2 + 1, loadSize) + numTrucks(numCrates/2, loadSize);
     return 2 * numTrucks(numCrates/2, loadSize);
